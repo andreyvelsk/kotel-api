2aca51b6a143084e30eb3fac5cfe7e9b93f29e23
andrey_velsk <andrey_velsk@mail.ru>
2018-11-15 23:21:46 +0300

    add sensors choose

--- getchartdata.php
+++ getchartdata.php
@@ -2,12 +2,16 @@
 include 'dbconnect.php';
 
 $interval = $_GET['interval'] ?? 3;
-$sensor =  $_GET['sensor'];
+$sensor =  $_GET['sensor'] ?? [1];
 
-$sql = "";
+$final = array();
+$dataset = array();
+$dataset_data = array();
 
 for ($i = 0; $i < count($sensor); $i++){
-    $sql = $sql . "
+    $data = array();
+    $labels = array();
+    $sql = "
     SELECT id_sensor, value, vdatetime FROM ( 
     SELECT @row := @row +1 AS rownum, id_sensor, value, vdatetime
     FROM (
@@ -18,44 +22,27 @@ for ($i = 0; $i < count($sensor); $i++){
     ) tmp
     WHERE rownum MOD 5 = 0
     ";
+
     // rownum mod 5 - выбираем каждую 5 строчку
-    if (count($sensor) > 1 && $i != count($sensor) - 1 ){
-        $sql = $sql."
-        UNION
-        ";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            array_push($labels, $row['vdatetime']);
+            array_push($data, $row['value']);
+            }
+
+            $dataset_data['label'] = 't_pod'.$i;
+            $dataset_data['data'] = $data;
+            array_push($dataset, $dataset_data);
     }
 
-}
-
-$result = mysqli_query($conn, $sql);
-
-$final = array();
-$labels = array();
-$data = array();
-$dataset = array();
-$dataset1 = array();
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        array_push($labels, $row['vdatetime']);
-        $data[$row['id_sensor']] = array();
-        array_push($data[$row['id_sensor']], $row['value']);
-    }
-    print_r($data);
-    $dataset1['label'] = 't_pod';
-    $dataset1['data'] = $data;
-    array_push($dataset, $dataset1);
-    
     $final['labels'] = $labels;
+}    
     $final['datasets'] = $dataset;
 
 	$json = json_encode($final); 
 
-	// echo $json;
-} else {
-    echo "[]";
-}
+     echo $json;
 
 
 
